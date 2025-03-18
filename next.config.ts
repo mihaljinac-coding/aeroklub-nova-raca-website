0710bae03cba2bda4a231400d886237eff30c42c
Viktorija Sabo <viktorija1sabo@outlook.com>
2025-03-18 21:11:30 +0100

    config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export'
+  output: 'export',
+  assetPrefix: isProd ? '/aeroklub-nova-raca-website/' : '',
+  basePath: isProd ? '/aeroklub-nova-raca-website' : '',
 };
 
 export default nextConfig;
